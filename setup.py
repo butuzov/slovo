1c79218fc0cb6c06b21bf0b7e9fca7ae24d57943
Oleg Butuzov <butuzov@made.ua>
2020-03-16 15:42:37 +0200

    chores(release); initial release

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+slovo
+~~~~~
+slovo's propose is mainly for making words.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="slovo",
+    version=__import__("slovo").__version__,
+    description="Making words",
+    author="Oleg Butuzov",
+    author_email="butuzov@made.ua",
+    platforms=["OS Independent"],
+    keywords=["words", "dictionalies"],
+    packages=find_packages(exclude=['tests']),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Topic :: Education",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
